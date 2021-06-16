f30e089a9c40d53f395434ff3642e57e1c174060
David Noronha <davidnd97@gmail.com>
2021-06-16 17:13:53 -0300

    projeto finalizado e conectado ao github

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,95 @@
+let timer;
+
+export default {
+    async login(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
+        });
+    },
+    async signup(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAR1VijwhT0Bw9oq2OySflClDIqwZ30pK0';
+
+        if(mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAR1VijwhT0Bw9oq2OySflClDIqwZ30pK0';
+        }
+
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
+
+        const responseData = await response.json();
+
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
+            throw error;
+        }
+        //pegando o tempo atual e adicionando a duração do token do firebase. para fazer o logout depois dessa duração de 1h.
+        const expiresIn = responseData.expiresIn * 1000;
+        const expirationDate = new Date().getTime() + expiresIn;
+
+        //'localStorage' salvando dados na api do navegador para salvar estado quando recarregar página
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+        localStorage.setItem('tokenExpiration', expirationDate);
+
+        timer = setTimeout(() => {
+            context.dispatch('autoLogout');
+        }, expiresIn);
+
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+        });
+    },
+    tryLogin(context) {//pegando dados armazenados no navegador 'localStorage'
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+        const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+        const expiresIn = +tokenExpiration - new Date().getTime();
+
+        if(expiresIn < 0) {
+            return;
+        }
+
+        timer = setTimeout(() => {
+            context.dispatch('autoLogout');
+        }, expiresIn);
+
+        if(token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+            })
+        }
+    },
+    logout(context) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        localStorage.removeItem('tokenExpiration');
+
+        clearTimeout(timer);
+
+        context.commit('setUser', {
+            token: null,
+            userId: null,
+        })
+    },
+    autoLogout(context) {
+        context.dispatch('logout');
+        context.commit('didAutoLogout');
+    }
+};
